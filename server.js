2249874e7cc77751f5e7bc07a3c67d8359890e95
helo nunes <helo.nunesfer@gmail.com>
2026-08-17 18:44:05 +0000

    edit: mudança no console

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ server.on('request', (req, res) => {
     console.log(`Requisição recebida! ${req.method} ${req.url}`);
 
     res.statusCode = 201
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.setHeader('Content-Type', 'application/json');
     res.end("Recurso Criado");    
 });
 
